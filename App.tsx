028baeec74c1af667e202bd4e47c126d7ef00676
Phaedrus Raznikov <praznikov@cerego.com>
2021-02-20 11:39:08 -0500

    Styling: Shadows

--- App.tsx
+++ App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
 
 export default function App() {
-  //return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "grey",
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          elevation: 20 // Android shadow
+        }}
+      ></View>
+    </View>
+  );
 }
