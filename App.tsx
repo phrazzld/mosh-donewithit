df16eedb6daef4e87e9a8e094f3bfe7887162b77
Phaedrus Raznikov <praznikov@cerego.com>
2021-02-20 11:41:23 -0500

    Styling: Padding and Margin

--- App.tsx
+++ App.tsx
@@ -15,11 +15,19 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          shadowColor: "grey",
-          shadowOffset: { width: 0, height: 10 },
-          shadowOpacity: 1,
-          shadowRadius: 10,
-          elevation: 20 // Android shadow
+          padding: 20,
+          paddingHorizontal: 10,
+          paddingLeft: 30
+        }}
+      >
+        <View style={{ backgroundColor: "gold", width: 50, height: 50 }}></View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+          margin: 20
         }}
       ></View>
     </View>
